3cb4f05f2a29151feeae68d5b747e5a61ddf8cf7
Priyesh Yadav <priyeshy217@gmail.com>
2018-06-10 18:17:07 +0530

    binding to custom property
    l-62

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-import { CommonModule } from '@angular/common';
-
+import { CockpitComponent } from './cockpit/cockpit.component';
+import { ServerElementComponent } from './server-element/server-element.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent
+    CockpitComponent,
+    ServerElementComponent
   ],
   imports: [
+    BrowserModule,
     FormsModule,
-    CommonModule,
-    BrowserModule
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
